25a646539e2477b2e7afad8a6396e10d47e8b446
paliwalpriyank <paliwal.priyank2001@gmail.com>
2016-08-18 18:38:12 -0400

    sign in changes

--- public/assets/components/main.js
+++ public/assets/components/main.js
@@ -1,12 +1,19 @@
 'use strict';
 var myapp = angular.module('blogApp');
-myapp.controller('headerController',['$scope','$location', function($scope,$location){
-      $scope.message='';
+myapp.controller('headerController',['$scope','$location','Auth', function($scope,$location,Auth){
+      $scope.message = '';
+      $scope.isLoggedIn = false;
       $scope.register=function(){
-      $location.path( "/register" );
+        $location.path( "/register" );
       };
       $scope.signIn=function(){
-      $location.path( "/" );
+        $location.path( "/" );
       };
-
+      $scope.signOut=function(){
+        Auth.logOut();
+        $location.path( "/" );
+      };
+      $scope.enableAuth=function(){
+        return Auth.isLoggedIn();
+      };
 }]);
