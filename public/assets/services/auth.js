3dfed3d11375c08c4abb80366f0a8bea27cf9c18
paliwalpriyank <paliwal.priyank2001@gmail.com>
2016-08-24 23:44:48 -0400

    autherization changes

--- public/assets/services/auth.js
+++ public/assets/services/auth.js
@@ -2,10 +2,12 @@
 var myapp = angular.module('blogApp')
 .factory('Auth', function(){
 var user;
-
 return{
     setUser : function(aUser){
         user = aUser;
+    },
+    logOut: function(){
+      user = undefined;
     },
     isLoggedIn : function(){
         return(user)? user : false;
